b3aa48d98c5a3ba2944afe1d468241b24e940b33
christof <christof.sirk@gmail.com>
2025-01-01 20:31:20 +0100

    Set shiftwidth to 4 and set number for line numbers

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 require("config.lazy")
 
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>xl", ":.lua<CR>")
 vim.keymap.set("v", "<space>xl", ":lua<CR>")
