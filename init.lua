44f3150b43d1f845ed582b2f9ad802928715f36a
christof <christof.sirk@gmail.com>
2025-01-01 20:32:45 +0100

    Apply new shiftwidth

--- init.lua
+++ init.lua
@@ -11,10 +11,10 @@ vim.keymap.set("v", "<space>xl", ":lua<CR>")
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank {higroup='Visual', timeout=300}
-	end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank {higroup='Visual', timeout=300}
+  end,
 })
 
