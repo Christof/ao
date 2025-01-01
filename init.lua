ce34e0d7c0fb244f099e6bef401f85615d6d150e
christof <christof.sirk@gmail.com>
2025-01-01 19:58:18 +0100

    Highlight when yanking text

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>xl", ":.lua<CR>")
 vim.keymap.set("v", "<space>xl", ":lua<CR>")
+
+-- Highlight when yanking text
+-- See `:help vim.highlight.on_yank()
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank {higroup='Visual', timeout=300}
+	end,
+})
+
