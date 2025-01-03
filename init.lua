fcd2711f7cc7defad1a8a031f1712c7dd2423452
christof <christof.sirk@gmail.com>
2025-01-03 21:00:42 +0100

    Some lsp mappings

--- init.lua
+++ init.lua
@@ -6,10 +6,12 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 
-
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>xl", ":.lua<CR>")
 vim.keymap.set("v", "<space>xl", ":lua<CR>")
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gsh", vim.lsp.buf.signature_help)
+vim.keymap.set("n", "<leader>", vim.lsp.buf.definition)
 
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()
