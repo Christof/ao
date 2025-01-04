079f30f203fc22754ac66c5795c37e4176c5ddbc
christof <christof.sirk@gmail.com>
2025-01-04 18:26:11 +0100

    Change <C-P> to show git files if in repo or all files if not

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -11,12 +11,29 @@ return {
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set("n", "<C-P>", builtin.find_files)
+      local is_inside_work_tree = {}
+
+      local project_files = function()
+        local opts = {} -- define here if you want to define something
+
+        local cwd = vim.fn.getcwd()
+        if is_inside_work_tree[cwd] == nil then
+          vim.fn.system("git rev-parse --is-inside-work-tree")
+          is_inside_work_tree[cwd] = vim.v.shell_error == 0
+        end
+
+        if is_inside_work_tree[cwd] then
+          builtin.git_files(opts)
+        else
+          builtin.find_files(opts)
+        end
+      end
+
+      vim.keymap.set("n", "<C-P>", project_files)
       vim.keymap.set("n", "<leader>p", builtin.find_files)
       vim.keymap.set("n", "<leader>en", function()
         builtin.find_files { cwd = vim.fn.stdpath("config") }
       end)
-      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
       vim.keymap.set("n", "<leader>f", function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
       end)
