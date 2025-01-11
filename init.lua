e216b48cb915c405d0226b4f2df3573f77399001
Christof <sirchb01@gmail.com>
2025-01-11 20:59:56 +0100

    Remove wrong keymap

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.keymap.set("n", "<space>xl", ":.lua<CR>")
 vim.keymap.set("v", "<space>xl", ":lua<CR>")
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gsh", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<leader>", vim.lsp.buf.definition)
 
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()
