2d7240124dbf437c11e475171990f51b85c9011e
Daniel Csuk <daniel.csuk@skills-lab.com>
2025-01-12 17:02:45 +0100

    Add mason to install LSPs

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -12,6 +12,16 @@ return {
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
+        -- Automatically install LSPs and related tools to stdpath for Neovim
+        -- Mason must be loaded before its dependents so we need to set it up here.
+        -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+        { 'williamboman/mason.nvim', opts = {} },
+        'williamboman/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+        -- Useful status updates for LSP.
+        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+        { 'j-hui/fidget.nvim',       opts = {} },
       }
     },
     config = function()
